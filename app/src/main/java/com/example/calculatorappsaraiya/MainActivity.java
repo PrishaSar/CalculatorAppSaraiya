e279a568fc9b30936f8629ca96091aa4e8017cb4
PrishaSar <113718020+PrishaSar@users.noreply.github.com>
2023-09-06 11:05:03 -0500

    changed resource files

--- app/src/main/java/com/example/calculatorappsaraiya/MainActivity.java
+++ app/src/main/java/com/example/calculatorappsaraiya/MainActivity.java
@@ -11,4 +11,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    private void Calculate(){
+        //get text form input (learned)
+        int in1 = 5;
+        int in2 = 10;
+        //find operation
+        //calc (learned)
+        //input in output box
+    }
 }
