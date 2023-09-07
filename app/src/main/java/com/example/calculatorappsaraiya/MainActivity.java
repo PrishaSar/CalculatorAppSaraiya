0dfc15a405b4a6eb58081c82bf47c534bda7a7b0
PrishaSar <113718020+PrishaSar@users.noreply.github.com>
2023-09-07 11:04:33 -0500

    finished basic layout and colors
    also started coding functionality - in the process of making each operation button a version of submit with onClick listener.

--- app/src/main/java/com/example/calculatorappsaraiya/MainActivity.java
+++ app/src/main/java/com/example/calculatorappsaraiya/MainActivity.java
@@ -3,21 +3,32 @@ package com.example.calculatorappsaraiya;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    //https://stackoverflow.com/questions/25905086/multiple-buttons-onclicklistener-android
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    private void Calculate(){
-        //get text form input (learned)
-        int in1 = 5;
-        int in2 = 10;
+
+    @Override
+    public void onClick(View v){
+        //get numbers from edit text and convert to Integer
+        EditText in1 = findViewById(R.id.editTextInput1);
+        EditText in2 = findViewById(R.id.editTextInput2);
+
+        int int1 = Integer.valueOf(in1.getText().toString());
+        int int2 = Integer.valueOf(in2.getText().toString());
+
         //find operation
         //calc (learned)
         //input in output box
     }
+
+    
 }
