3dee850bd5e697585a35cb8a06e6a8e8ab878377
Prisha <113718020+PrishaSar@users.noreply.github.com>
2023-09-11 16:23:19 -0500

    got switch screens to work!
    - need to work on the overall layout

--- app/src/main/java/com/example/calculatorappsaraiya/CalcHistory.java
+++ app/src/main/java/com/example/calculatorappsaraiya/CalcHistory.java
@@ -1,27 +1,28 @@
 package com.example.calculatorappsaraiya;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.appcompat.app.AppCompatActivity;
+public class CalcHistory extends AppCompatActivity {
 
-public class HistoryClass extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.class_history);
-
-        //inside the onCreate, get intent
+        setContentView(R.layout.activity_calc_history);
 
         Intent intent = getIntent();
         //name has to match when sending package
         String hist = intent.getStringExtra("HISTORY");
-        TextView hisTV = findViewById(R.id.history);
+        TextView hisTV = findViewById(R.id.calcHistory);
         hisTV.setText(hist);
-
     }
 
+    public void continueCalc(View v){
+        Intent back = new Intent(this, MainActivity.class);
+        startActivity(back);
+    }
 }
--- app/src/main/java/com/example/calculatorappsaraiya/MainActivity.java
+++ app/src/main/java/com/example/calculatorappsaraiya/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -90,7 +91,7 @@ public class MainActivity extends AppCompatActivity{
 
 
     public void showHistory(View v){
-        Intent intent = new Intent(this, HistoryClass.class);
+        Intent intent = new Intent(this, CalcHistory.class);
         intent.putExtra("HISTORY", his);
         startActivity(intent);
     }
